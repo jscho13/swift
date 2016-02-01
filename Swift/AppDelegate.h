a51a22d2a376fb2b3d0706feda292827036acb98
Joseph Cho <jscho13@gmail.com>
2016-02-01 18:27:13 -0500

    Initial Commit

---
+++ Swift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Swift
+//
+//  Created by Joseph Cho on 2/1/16.
+//  Copyright © 2016 Joseph Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Swift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swift
+//
+//  Created by Joseph Cho on 2/1/16.
+//  Copyright © 2016 Joseph Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
